039ccdf6997a14c0964c793c2192df3df2620f31
ylee <leeyn.shun@gmail.com>
2024-11-28 18:44:36 +0200

    Add getLocCount() to count number of countries performed in Add getRelationsClean() to display locations in relations properly

--- getArtists.go
+++ getArtists.go
@@ -1,15 +1,21 @@
 package main
 
+import (
+	"strings"
+)
+
 type artistData struct {
-	Image        string
-	Name         string
-	Members      []string
-	MembersCount int
-	CreationDate int
-	FirstAlbum   string
-	Locations    []string
-	Performances int
-	Relations    map[string][]string
+	Index          int
+	Image          string
+	Name           string
+	Members        []string
+	MembersCount   int
+	CreationDate   int
+	FirstAlbum     string
+	Locations      map[string][]string
+	LocationsCount int
+	Performances   int
+	Relations      map[string][]string
 }
 
 var artistsData []artistData
@@ -18,15 +24,54 @@ func getArtist() {
 	grabAPI()
 	for i := range artists {
 		artistsData = append(artistsData, artistData{
-			Image:        artists[i].Image,
-			Name:         artists[i].Name,
-			Members:      artists[i].Members,
-			MembersCount: len(artists[i].Members),
-			CreationDate: artists[i].CreationDate,
-			FirstAlbum:   artists[i].FirstAlbum,
-			Locations:    locLst.Locations[i].Locations,
-			Performances: len(dateLst.Dates[i].Dates),
-			Relations:    relLst.Relations[i].DatesLocations,
+			Index:          artists[i].Id,
+			Image:          artists[i].Image,
+			Name:           artists[i].Name,
+			Members:        artists[i].Members,
+			MembersCount:   len(artists[i].Members),
+			CreationDate:   artists[i].CreationDate,
+			FirstAlbum:     artists[i].FirstAlbum,
+			Locations:      getRelationsClean(relLst.Relations[i].DatesLocations),
+			LocationsCount: getLocCount(locLst.Locations[i].Locations),
+			Performances:   len(dateLst.Dates[i].Dates),
+			Relations:      relLst.Relations[i].DatesLocations,
 		})
 	}
+}
+
+func getLocCount(locations []string) int {
+
+	locLst := make(map[string]string)
+	for _, loc := range locations {
+		splited := strings.Split(loc, "-")
+
+		if len(splited) == 2 {
+			locLst[splited[1]] = splited[0]
+		}
+	}
+	return len(locLst)
+}
+
+func getRelationsClean(ogRelLst map[string][]string) map[string][]string {
+	newRelLst := make(map[string][]string)
+
+	for key, value := range ogRelLst {
+		newKey := strings.ReplaceAll(strings.ReplaceAll(key, "_", " "), "-", ", ")
+		newKey = capitalize(newKey)
+
+		newRelLst[newKey] = value
+	}
+
+	return newRelLst
+}
+
+func capitalize(s string) (result string) {
+	splited := strings.Split(s, " ")
+	for _, word := range splited {
+		if word == "usa" || word == "uk" {
+			word = strings.ToUpper(word)
+		}
+		result += strings.ToUpper(string(word[0])) + word[1:] + " "
+	}
+	return result[:len(result)-1]
 }
