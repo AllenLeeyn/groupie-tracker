44b2eaf0726b42d583c7d19487e83f3397af69be
atouba <anass.touba@gritlab.ax>
2024-12-02 01:52:50 +0200

    remove filter code (filter version of the code is pushed to filter-v1 branch
    which I'm going to stylize and fix some issues later)

--- handlers.go
+++ handlers.go
@@ -2,53 +2,48 @@ package main
 
 import (
 	"net/http"
+	"slices"
 	"sort"
 )
 
-// // homeHandler() checks the http request methods and url
-// // and provide the correct response.
-// func homeHandler(w http.ResponseWriter, req *http.Request) {
-// 	for index, artist := range artistsLst {
-// 		if req.URL.Path[1:] == artist.Name &&
-// 			req.Method == http.MethodGet {
-// 			artistHandler(w, index)
-// 			return
-// 		}
-// 	}
-// 	if req.URL.Path != "/" {
-// 		http.Error(w, "404 not found", http.StatusNotFound)
-// 		return
-// 	}
-// 	order := "▼"
-// 	sortCriteria := "default"
-// 	sortLst(sortCriteria)
+// arrangeArtists checks for method error and sorts/filter artists
+func arrangeArtists(w http.ResponseWriter, req *http.Request) error {
+	// var homePage *listPage
+	var newArtistsLst []artist = slices.Clone(artistsLst)
 
-// 	// POST method is used for sorting list.
-// 	// Invalid request is ignore and use default settings.
-// 	if req.Method == http.MethodPost {
-// 		sortCriteria = req.FormValue("sort")
-// 		if sortCriteria != "creation_date" && sortCriteria != "name" {
-// 			sortCriteria = "default"
-// 		}
-// 		sortLst(sortCriteria)
-// 		pageOrder := req.FormValue("switch-order")
-// 		if pageOrder == "▼" {
-// 			order = "▲"
-// 			revLst()
-// 		} else if pageOrder == "▲" {
-// 			order = "▼"
-// 		}
-// 	} else if req.Method != http.MethodGet {
-// 		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
-// 		return
-// 	}
+	checkErr(req.ParseForm())
+	order, sortCriteria, err := sortArtists(w, req, newArtistsLst)
+	checkErr(err)
+	homePage = &listPage{
+		Artists: newArtistsLst,
+		Order:   order,
+		SortBy:  sortCriteria,
+	}
+	return nil
+}
 
-// 	homePage := &listPage{
-// 		Artists: artistsLst,
-// 		SortBy:  sortCriteria,
-// 		Order:   order}
-// 	indexTmpl.Execute(w, homePage)
-// }
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	// fmt.Println("ARTISTSDATA:", artistsData)
+	for index, artist := range artistsLst {
+		if req.URL.Path[1:] == artist.Name &&
+			req.Method == http.MethodGet {
+			artistHandler(w, index)
+			return
+		}
+	}
+	if req.URL.Path != "/" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	err := arrangeArtists(w, req)
+	if err != nil {
+		errPage := err.(errorPage) // type assertion
+		http.Error(w, errPage.errorMsg, errPage.errorCode)
+		return
+	}
+	indexTmpl.Execute(w, homePage)
+}
 
 // artistHandler() generates the html response for an artist
 func artistHandler(w http.ResponseWriter, index int) {
--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"regexp"
-	"slices"
-	"strconv"
-	"strings"
 	"text/template"
 )
 
@@ -18,14 +13,6 @@ type listPage struct {
 	Artists []artist
 	SortBy  string
 	Order   string
-
-	// filters
-	NbChecked  []string
-	DateFA     string
-	Locations  []string
-	EarliestDt string
-	LatestDt   string
-	ApplyRange string
 }
 
 var homePage *listPage = &listPage{}
@@ -56,36 +43,6 @@ func main() {
 	http.ListenAndServe(port, nil)
 }
 
-func checkFADate(date []string) error {
-	if len(date) == 0 {
-		return errors.New("invalid date format")
-	}
-	isMatch, _ := regexp.Match("([0-9]{2}-[0-9]{2}-[0-9]{4})", []byte(date[0]))
-	if len(date[0]) != 10 || !isMatch {
-		return errors.New("invalid date format")
-	}
-	return nil
-}
-
-func checkGetFADate(dateArr []string) string {
-	var date string
-
-	err := checkFADate(dateArr)
-	if err == nil {
-		date = dateArr[0]
-	} else {
-		date = ""
-	}
-	return date
-}
-
-func checkGetLocations(locationsArr []string) []string {
-	if len(locationsArr) == 0 {
-		return []string{}
-	}
-	return strings.Fields(locationsArr[0])
-}
-
 func sortArtists(w http.ResponseWriter, req *http.Request, arr []artist) (string, string, error) {
 	order := "▼"
 	sortCriteria := "default"
@@ -111,109 +68,4 @@ func sortArtists(w http.ResponseWriter, req *http.Request, arr []artist) (string
 		return "", "", errors.New("http error")
 	}
 	return order, sortCriteria, nil
-}
-
-// arrangeArtists checks for method error and sorts/filter artists
-// filter by membersNb, dataFA
-func arrangeArtists(w http.ResponseWriter, req *http.Request) error {
-	// var homePage *listPage
-	var newArtistsLst []artist = slices.Clone(artistsLst)
-
-	checkErr(req.ParseForm())
-	// make a new copy, so when the artists' array is filtered, the artists
-	// who are not displayed aren't lost for the whole execution of the program
-	membersNb := req.Form["members number"]
-	dateFA := checkGetFADate(req.Form["first album date"]) // FA: first album
-	locations := checkGetLocations(req.Form["locations"])
-	rangeValue := req.Form["range"]
-	applyRange := req.Form["applyRange"]
-	fmt.Println("range value:", rangeValue)
-	if (len(req.Form["submit button"]) == 1 && req.Method != "POST") ||
-		((len(req.Form["submit button"]) == 0 && len(req.Form["sort"]) == 0 && len(req.Form["switch-order"]) == 0) && req.Method != "GET") {
-		return errorPage{405, "405 method not allowed"}
-	}
-	if len(membersNb) != 0 {
-		newArtistsLst = filterArtists(newArtistsLst, membersNb)
-	}
-	if dateFA != "" {
-		newArtistsLst = filter(newArtistsLst, dateFA, compareFADate)
-	}
-	if len(locations) != 0 {
-		newArtistsLst = filterLocations(newArtistsLst, locations)
-	}
-	if len(applyRange) != 0 {
-		newArtistsLst = filter(newArtistsLst, rangeValue[0], compareCreationDate)
-	}
-	// sortAlph(newArtistsLst)
-	order, sortCriteria, err := sortArtists(w, req, newArtistsLst)
-	checkErr(err)
-	homePage = &listPage{
-		Artists:    newArtistsLst,
-		NbChecked:  membersNb,
-		DateFA:     dateFA,
-		Locations:  locations,
-		Order:      order,
-		SortBy:     sortCriteria,
-		EarliestDt: strconv.Itoa(earliestCreationDate(newArtistsLst)),
-		LatestDt:   strconv.Itoa(latestCreationDate(newArtistsLst)),
-	}
-	// homePage.Artists = newArtistsLst
-	// homePage.NbChecked = membersNb
-	// homePage.DateFA = dateFA
-	// homePage.Locations = locations
-	// homePage.Order = order
-	// homePage.SortBy = sortCriteria
-	return nil
-}
-
-func homeHandler(w http.ResponseWriter, req *http.Request) {
-	// fmt.Println("ARTISTSDATA:", artistsData)
-	for index, artist := range artistsLst {
-		if req.URL.Path[1:] == artist.Name &&
-			req.Method == http.MethodGet {
-			artistHandler(w, index)
-			return
-		}
-	}
-	if req.URL.Path != "/" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	// if req.Method != "POST" {
-	// 	newArtistsLst = slices.Clone(artistsLst)
-	// }
-
-	err := arrangeArtists(w, req)
-	if err != nil {
-		errPage := err.(errorPage) // type assertion
-		http.Error(w, errPage.errorMsg, errPage.errorCode)
-		return
-	}
-	indexTmpl.Execute(w, homePage)
-}
-
-func earliestCreationDate(arr []artist) int {
-	if len(arr) == 0 {
-		return -1
-	}
-	earliestDate := arr[0].CreationDate
-	for _, artist := range arr {
-		if earliestDate > artist.CreationDate {
-			earliestDate = artist.CreationDate
-		}
-	}
-	return earliestDate
-}
-
-func latestCreationDate(arr []artist) int {
-	if len(arr) == 0 {
-		return -1
-	}
-	var latestDate int
-	for _, artist := range arr {
-		if latestDate < artist.CreationDate {
-			latestDate = artist.CreationDate
-		}
-	}
-	return latestDate
 }
